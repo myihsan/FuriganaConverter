c2d940f193d0c7f8481be1c7f30505108907fdf5
Jierong Li <ihsan.lee@gmail.com>
2019-12-05 02:40:52 +0900

    Make the test pass

--- FuriganaConverter/Data/Remote/GooFuriganaConverterRemoteAPI.swift
+++ FuriganaConverter/Data/Remote/GooFuriganaConverterRemoteAPI.swift
@@ -10,18 +10,33 @@ import Foundation
 
 class GooFuriganaConverterRemoteAPI: FuriganaConverterRemoteAPI {
 
-    private let requestURL = URL(string: "https://labs.goo.ne.jp/api/hiragana")!
     let session: URLSession
+
+    private let requestURL = URL(string: "https://labs.goo.ne.jp/api/hiragana")!
+    private let appID: String
 
     init(session: URLSession) {
         self.session = session
+        let privateInfosPath = Bundle.main.path(forResource: "PrivateInfo", ofType: "plist")!
+        let privateInfos = NSDictionary(contentsOfFile: privateInfosPath)!
+        // Invalid app_id is better then crash
+        // if there is a proper alert like "Please contact the developer.".
+        appID = privateInfos["AppID"] as? String ?? ""
     }
 
     func convert(
         _ japaneseString: String,
         completionHandler: @escaping (Result<String, RemoteAPIError>) -> Void
     ) -> URLSessionDataTask {
-        let request = URLRequest(url: requestURL)
+        var request = URLRequest(url: requestURL)
+        let httpBodyJSONObject = [
+            "app_id": appID,
+            "sentence": japaneseString,
+            "output_type": "hiragana"
+        ]
+        // Never failed since httpBodyJSONObject is a [String: String]
+        // swiftlint:disable:next force_try
+        request.httpBody = try!  JSONSerialization.data(withJSONObject: httpBodyJSONObject)
         return session.dataTask(with: request) { _, _, _ in }
     }
 }
