437730afde15ef1fa239f953794d3195149d493a
Jierong Li <ihsan.lee@gmail.com>
2019-12-05 02:20:39 +0900

    Add GooFuriganaConverterRemoteAPI with a test
    Test if GooFuriganaConverterRemoteAPI conforms to FuriganaConverterRemoteAPI.
    Remove an autogenerated test class.

---
+++ FuriganaConverter/Data/Remote/GooFuriganaConverterRemoteAPI.swift
@@ -0,0 +1,9 @@
+//
+//  GooFuriganaConverterRemoteAPI.swift
+//  FuriganaConverter
+//
+//  Created by Jierong Li on 2019/12/05.
+//  Copyright © 2019 Jierong Li. All rights reserved.
+//
+
+class GooFuriganaConverterRemoteAPI {}
---
+++ FuriganaConverterTests/Data/Remote/GooFuriganaConverterRemoteAPITests.swift
@@ -0,0 +1,27 @@
+//
+//  GooFuriganaConverterRemoteAPITests.swift
+//  FuriganaConverterTests
+//
+//  Created by Jierong Li on 2019/12/05.
+//  Copyright © 2019 Jierong Li. All rights reserved.
+//
+
+@testable import FuriganaConverter
+import XCTest
+
+class GooFuriganaConverterRemoteAPITests: XCTestCase {
+
+    var sut: GooFuriganaConverterRemoteAPI!
+
+    override func setUp() {
+        sut = GooFuriganaConverterRemoteAPI()
+    }
+
+    override func tearDown() {
+        sut = nil
+    }
+
+    func test_conformsToFuriganaConverterRemoteAPI() {
+        XCTAssertTrue((sut as AnyObject) is FuriganaConverterRemoteAPI)
+    }
+}
